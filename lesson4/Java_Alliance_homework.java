9057a61ee15d76170385c29392c816bd31b7c230
alekarios <33423935+alekarios@users.noreply.github.com>
2017-11-09 09:43:49 +0200

    Add files via upload

---
+++ lesson4/Java_Alliance_homework.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java_alliance_homework;
+
+/**
+ *
+ * @author Alekarios
+ */
+
+
+import java.util.Scanner;
+public class Java_Alliance_homework {
+
+    public static void main(String[] args) {
+        
+        
+        
+        Scanner scan = new Scanner(System.in);
+        
+        while(true){
+            
+        
+        System.out.println("Type your 1st number");
+        int num1 = scan.nextInt();
+        System.out.println("Type your 2nd number");
+        int num2 = scan.nextInt();
+        System.out.println("Type your 3rd number");
+        int num3 = scan.nextInt();
+        
+        if (num2 == 3*num1 && num3 > num1 + num2){
+          System.out.println("Succesfull combination: "+num1+" "+num2+" "+num3);
+          break;
+        }
+        
+        else{
+            System.out.println("Try again");
+        }
+        
+       
+    }
+    
+}
+}
